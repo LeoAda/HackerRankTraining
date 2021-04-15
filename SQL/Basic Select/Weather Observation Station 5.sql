0b717e1df18b5b3e310260f7003c1be1c24ed7e9
LeoAda <69049724+LeoAda@users.noreply.github.com>
2021-04-15 19:29:40 +0200

    Create Weather Observation Station 5.sql

---
+++ SQL/Basic Select/Weather Observation Station 5.sql
@@ -0,0 +1,8 @@
+##https://www.hackerrank.com/challenges/weather-observation-station-5/
+
+select CITY, length(CITY) from STATION ##We select the city name and length
+order by length(CITY),CITY asc ##we want the smallest value sorted alphabetically, so we sort the list by length and name in ascending order
+limit 1;  ##we take the first value
+select CITY, length(CITY) from STATION 
+order by length(CITY) desc #same here but we want the greatest value so we order in descending order
+limit 1
