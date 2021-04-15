ee96ceae300dd98bb68ad7774bc6f3f3b3426b38
LeoAda <69049724+LeoAda@users.noreply.github.com>
2021-04-15 21:23:28 +0200

    Create Weather Observation Station 11.sql

---
+++ SQL/Basic Select/Weather Observation Station 11.sql
@@ -0,0 +1,6 @@
+##https://www.hackerrank.com/challenges/weather-observation-station-11/
+
+select distinct CITY from STATION
+where CITY REGEXP "^[^aeiou]|[^aeiou]$"
+
+##the | is or and ^ inside [] means not
