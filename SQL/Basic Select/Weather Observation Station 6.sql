71677daca5985fce102048abc1975271c1c6a29b
LeoAda <69049724+LeoAda@users.noreply.github.com>
2021-04-15 20:41:52 +0200

    Update Weather Observation Station 6.sql

--- SQL/Basic Select/Weather Observation Station 6.sql
+++ SQL/Basic Select/Weather Observation Station 6.sql
@@ -1,7 +1,7 @@
 ##https://www.hackerrank.com/challenges/weather-observation-station-6/
 
 select distinct CITY from STATION 
-where CITY REGEXP "^[aeiou]."
+where CITY REGEXP "^[aeiou]"
 
 ##we use REGEXP to search the vowels in the city name,
-## ^ means the beginning of the string, [...] the list, . means match any char between list and word
+## ^ means the beginning of the string, [...] the list
