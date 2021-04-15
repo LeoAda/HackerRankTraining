aa11e7c18049342e45811a2d86acadd3af2a70bc
LeoAda <69049724+LeoAda@users.noreply.github.com>
2021-04-15 20:41:21 +0200

    Create Weather Observation Station 7.sql

---
+++ SQL/Basic Select/Weather Observation Station 7.sql
@@ -0,0 +1,6 @@
+##https://www.hackerrank.com/challenges/weather-observation-station-7/
+
+select distinct CITY from STATION 
+where CITY REGEXP "[aeiou]$"
+
+##same as before but we replace the ^ by a $ to match the end
