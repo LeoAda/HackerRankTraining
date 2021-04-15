0a35bdd36714087fae85f75d0e6d69c5efc3ed20
LeoAda <69049724+LeoAda@users.noreply.github.com>
2021-04-15 21:37:24 +0200

    Create Higher Than 75 Marks.sql

---
+++ SQL/Basic Select/Higher Than 75 Marks.sql
@@ -0,0 +1,5 @@
+##https://www.hackerrank.com/challenges/more-than-75-marks/
+
+select Name from STUDENTS where marks>75 order by RIGHT(name,3),ID
+
+##To sort by 3 lasts letters we use right(name,3) and if the same, by id
