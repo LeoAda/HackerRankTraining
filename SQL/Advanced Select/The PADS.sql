614c5539767ec3df033be1e4cef149f685f0c0c4
LeoAda <69049724+LeoAda@users.noreply.github.com>
2021-04-16 22:37:07 +0200

    Update The PADS.sql

--- SQL/Advanced Select/The PADS.sql
+++ SQL/Advanced Select/The PADS.sql
@@ -1,5 +1,5 @@
-##https://www.hackerrank.com/challenges/the-pads/
+--https://www.hackerrank.com/challenges/the-pads/
 
-select CONCAT(Name,'(',LEFT(Occupation, 1),')') from OCCUPATIONS ORDER BY Name; ##on récupere la premoere lettre à gauche
+select CONCAT(Name,'(',LEFT(Occupation, 1),')') from OCCUPATIONS ORDER BY Name; --on récupere la premoere lettre à gauche
 select 'There are a total of', Count(OCCUPATION), concat(lower(Occupation),"s.") from OCCUPATIONS 
 Group by Occupation ORDER BY Count(Occupation),Occupation;
