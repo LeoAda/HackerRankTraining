32456aa9faa72d6873836c8a35fec2c1371e3bbd
LeoAda <69049724+LeoAda@users.noreply.github.com>
2021-04-16 23:03:26 +0200

    Update New Companies.sql

--- SQL/Advanced Select/New Companies.sql
+++ SQL/Advanced Select/New Companies.sql
@@ -1,2 +1,6 @@
 --https://www.hackerrank.com/challenges/the-company/
 
+select company_code,founder,(select count(lead_Manager_code) from Lead_Manager where company_code = company_code) from Company 
+order by CONVERT(SUBSTRING_INDEX(company_code,'C',-1),UNSIGNED)
+
+WIP
