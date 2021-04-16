1a81679dc3d3168db2930cbfde04e5464e2f52ee
LeoAda <69049724+LeoAda@users.noreply.github.com>
2021-04-16 22:39:36 +0200

    Update Weather Observation Station 3.sql

--- SQL/Basic Select/Weather Observation Station 3.sql
+++ SQL/Basic Select/Weather Observation Station 3.sql
@@ -1,3 +1,3 @@
-##https://www.hackerrank.com/challenges/weather-observation-station-3/
+--https://www.hackerrank.com/challenges/weather-observation-station-3/
 
 select DISTINCT(CITY) from STATION where ID % 2 = 0
