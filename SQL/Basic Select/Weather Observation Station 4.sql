f98dedc20aeef21793e26b9645e89ee0a1eaa57d
LeoAda <69049724+LeoAda@users.noreply.github.com>
2021-04-16 22:39:46 +0200

    Update Weather Observation Station 4.sql

--- SQL/Basic Select/Weather Observation Station 4.sql
+++ SQL/Basic Select/Weather Observation Station 4.sql
@@ -1,3 +1,3 @@
-##https://www.hackerrank.com/challenges/weather-observation-station-4/
+--https://www.hackerrank.com/challenges/weather-observation-station-4/
 
 SELECT COUNT(CITY) - COUNT(DISTINCT CITY) FROM STATION
