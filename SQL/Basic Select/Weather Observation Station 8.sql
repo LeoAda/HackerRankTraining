55a232df18148e05233cf4b66ff868169e7a31b3
LeoAda <69049724+LeoAda@users.noreply.github.com>
2021-04-16 22:40:43 +0200

    Update Weather Observation Station 8.sql

--- SQL/Basic Select/Weather Observation Station 8.sql
+++ SQL/Basic Select/Weather Observation Station 8.sql
@@ -1,4 +1,4 @@
-##https://www.hackerrank.com/challenges/weather-observation-station-8/
+--https://www.hackerrank.com/challenges/weather-observation-station-8/
 
 select distinct CITY from STATION 
 where CITY REGEXP "^[aeiou].*[aeiou]$"
