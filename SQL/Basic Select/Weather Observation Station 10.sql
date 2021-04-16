f77762aeec6be60a937211dc4c304fbc11036ae7
LeoAda <69049724+LeoAda@users.noreply.github.com>
2021-04-16 22:39:00 +0200

    Update Weather Observation Station 10.sql

--- SQL/Basic Select/Weather Observation Station 10.sql
+++ SQL/Basic Select/Weather Observation Station 10.sql
@@ -1,4 +1,4 @@
-##https://www.hackerrank.com/challenges/weather-observation-station-10/
+--https://www.hackerrank.com/challenges/weather-observation-station-10/
 
 select distinct CITY from STATION 
 where CITY NOT REGEXP "[aeiou]$"
