55dcf9acf76872a452ae46a60c307976fb43ce06
LeoAda <69049724+LeoAda@users.noreply.github.com>
2021-04-16 22:37:29 +0200

    Update Employee Salaries.sql

--- SQL/Basic Select/Employee Salaries.sql
+++ SQL/Basic Select/Employee Salaries.sql
@@ -1,3 +1,3 @@
-##https://www.hackerrank.com/challenges/salary-of-employees/
+--https://www.hackerrank.com/challenges/salary-of-employees/
 
 select name from Employee where salary > 2000 and months < 10 order by employee_id
