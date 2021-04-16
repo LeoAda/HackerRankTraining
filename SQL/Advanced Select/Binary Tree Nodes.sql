dd3f72e18b8ee50032989fafafdf2e4653a3de1c
LeoAda <69049724+LeoAda@users.noreply.github.com>
2021-04-16 17:14:53 +0200

    Update Binary Tree Nodes.sql

--- SQL/Advanced Select/Binary Tree Nodes.sql
+++ SQL/Advanced Select/Binary Tree Nodes.sql
@@ -1,5 +1,11 @@
-##https://www.hackerrank.com/challenges/binary-search-tree-1/
+--https://www.hackerrank.com/challenges/binary-search-tree-1/
 
-select * from BST order by P desc,N asc
+select N, 
+CASE
+    WHEN P = NULL THEN ' Root'
+    WHEN (SI N EST DANS P) THEN ' Leaf'
+    ELSE ' Inner'
+from BST 
+order by N asc
 
 WIP
