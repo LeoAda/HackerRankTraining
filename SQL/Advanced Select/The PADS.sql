04e09424df9c5565b0a98f21d64758275b33e98d
LeoAda <69049724+LeoAda@users.noreply.github.com>
2021-04-16 14:37:55 +0200

    Update The PADS.sql

--- SQL/Advanced Select/The PADS.sql
+++ SQL/Advanced Select/The PADS.sql
@@ -1,4 +1,3 @@
 select CONCAT(Name,'(',LEFT(Occupation, 1),')') from OCCUPATIONS ORDER BY Name;
-select distinct 'There are a total of', Count(*), Occupation,'.' from OCCUPATIONS Group by Occupation ORDER BY Count(Occupation),Occupation;
+select 'There are a total of', Count(OCCUPATION), concat(lower(Occupation),"s.") from OCCUPATIONS Group by Occupation ORDER BY Count(Occupation),Occupation;
 
-NOT FINISH
