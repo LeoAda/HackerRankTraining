6082da9f6aabd45d9784c62375edf62d523106cf
LeoAda <69049724+LeoAda@users.noreply.github.com>
2021-04-16 14:51:25 +0200

    Update The PADS.sql

--- SQL/Advanced Select/The PADS.sql
+++ SQL/Advanced Select/The PADS.sql
@@ -1,3 +1,5 @@
-select CONCAT(Name,'(',LEFT(Occupation, 1),')') from OCCUPATIONS ORDER BY Name;
-select 'There are a total of', Count(OCCUPATION), concat(lower(Occupation),"s.") from OCCUPATIONS Group by Occupation ORDER BY Count(Occupation),Occupation;
+##https://www.hackerrank.com/challenges/the-pads/
 
+select CONCAT(Name,'(',LEFT(Occupation, 1),')') from OCCUPATIONS ORDER BY Name; ##on récupere la premoere lettre à gauche
+select 'There are a total of', Count(OCCUPATION), concat(lower(Occupation),"s.") from OCCUPATIONS 
+Group by Occupation ORDER BY Count(Occupation),Occupation;
