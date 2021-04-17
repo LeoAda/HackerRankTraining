248a34f309329e952284efc17b18432bd6e0bc74
LeoAda <69049724+LeoAda@users.noreply.github.com>
2021-04-17 21:29:38 +0200

    Update New Companies.sql

--- SQL/Advanced Select/New Companies.sql
+++ SQL/Advanced Select/New Companies.sql
@@ -1,6 +1,10 @@
 --https://www.hackerrank.com/challenges/the-company/
 
-select company_code,founder,(select count(lead_Manager_code) from Lead_Manager where company_code = company_code) from Company 
-order by CONVERT(SUBSTRING_INDEX(company_code,'C',-1),UNSIGNED)
+select company_code, founder, (select count(distinct lead_manager_code) from Lead_Manager where company_code=Company.company_code),
+(select count(distinct senior_manager_code) from Senior_Manager where company_code=Company.company_code),
+(select count(distinct manager_code) from Manager where company_code=Company.company_code),
+(select count(distinct employee_code) from Employee where company_code=Company.company_code)
+from Company
+order by company_code
 
-WIP
+--join isn't allow in this exercise
