112c304232f3e6551066387ac37200b9e338bb2b
LeoAda <69049724+LeoAda@users.noreply.github.com>
2021-04-18 14:15:03 +0200

    Update Weather Observation Station 20.sql

--- SQL/Aggregation/Weather Observation Station 20.sql
+++ SQL/Aggregation/Weather Observation Station 20.sql
@@ -1,5 +1,7 @@
 --https://www.hackerrank.com/challenges/weather-observation-station-20/
 
-select round(LAT_N,4) from STATION order by LAT_N limit ((select count(*) from station)/2),1
+select round(RankTable.LAT_N,4)
+from (select LAT_N, row_number() over(order by LAT_N) row_num from STATION) as RankTable
+where RankTable.row_num=((select count(*) from STATION)+1)/2
 
-WIP
+--Here we create a table with the LAT_N and rank with a window and we look at the value at the 50% value
