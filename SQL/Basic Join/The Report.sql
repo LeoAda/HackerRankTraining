ffb53eab291e6dcb62815d8e78d2461aa565264a
LeoAda <69049724+LeoAda@users.noreply.github.com>
2021-04-19 21:46:17 +0200

    Update The Report.sql

--- SQL/Basic Join/The Report.sql
+++ SQL/Basic Join/The Report.sql
@@ -1,1 +1,5 @@
-WIP
+--https://www.hackerrank.com/challenges/the-report/
+
+select if(GRADE>=8,NAME,NULL),GRADE,MARKS from STUDENTS join GRADES
+where MARKS between Min_Mark and Max_Mark
+order by GRADE desc, NAME asc
